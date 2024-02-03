99b54dffa69d0ff80793fb9691a8e84f096dc020
IvakisSolo <158038497+IvakisSolo@users.noreply.github.com>
2024-02-03 18:50:22 +0200

    Finished homework for triangles

---
+++ Lecture4/Homework/possibleTriangle.java
@@ -0,0 +1,42 @@
+package Lecture4.Homework;
+
+import java.util.Scanner;
+
+public class possibleTriangle {
+    public static void main(String args[]) {
+        Scanner in = new Scanner(System.in);
+        System.out.print("Въведете първият ъгъл: ");
+        int a1 = in.nextInt();
+        System.out.print("Въведете вторият ъгъл: ");
+        int a2 = in.nextInt();
+        System.out.print("Въведете третият ъгъл: ");
+        int a3 = in.nextInt();
+        int angleSum = a1 + a2 + a3;
+
+        if (angleSum == 180 && a1 > 0 && a2 > 0 && a3 > 0) {
+            if (a1 < 90 && a2 < 90 && a3 < 90) {
+                 System.out.println("Триъгълникът е осроъгълен");
+             }
+             else if (a1 == 90 || a2 == 90 || a3 == 90) {
+                 System.out.println("Триъгълникът е правоъгълен");
+             }
+             else {
+                 System.out.println("Триъгълникът е тъпоъгълен");
+             }
+             if (a1 == a3 || a1 == a2 || a2 == a3) {
+                 System.out.println("Триъгълникът е равнобедрен");
+             }
+             else if (a1 == 60 && a1 == 60 && a2 == 60) {
+                 System.out.println("Триъгълникът е равностранен");
+             }
+             else {
+                 System.out.println("Тпиъгълникът е разностранен");
+
+             }
+        }
+        else {
+            System.out.println("Triangle not possible");
+        }
+    }
+}
+
