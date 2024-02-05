e707a9bda6cc5cc65c2774e2c086c8fa6d9da0f5
IvakisSolo <158038497+IvakisSolo@users.noreply.github.com>
2024-02-05 13:36:48 +0200

    Finished Homework 2

--- Lecture4/Homework2_workAge.java
+++ Lecture4/Homework2_workAge.java
@@ -1,8 +1,18 @@
 package Lecture4;
 
+import java.util.Scanner;
+
 public class Homework2_workAge {
 
     public static void main(String[] args) {
-
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Въведете вашата възраст: ");
+        int age = scanner.nextInt();
+        if (age >=16) {
+            System.out.println("Вашата възраст Ви позволява да работите");
+        }
+        else {
+            System.out.println("Вашата възраст не Ви позволява да работите");
+        }
     }
 }
