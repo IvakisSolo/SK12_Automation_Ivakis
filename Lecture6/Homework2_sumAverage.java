8b6856b3c411b2b94d903b16a5fbea0191cce9f0
IvakisSolo <158038497+IvakisSolo@users.noreply.github.com>
2024-02-10 14:01:29 +0200

    Completed homework 3

--- Lecture6/Homework2_sumAverage.java
+++ Lecture6/Homework2_sumAverage.java
@@ -8,15 +8,15 @@ public class Homework2_sumAverage {
         Scanner in = new Scanner(System.in);
 
         System.out.print("Въведете елемент 1: ");
-        double element1 = in.nextInt();
+        int element1 = in.nextInt();
 
         System.out.print("Въведете елемент 2: ");
-        double element2 = in.nextInt();
+        int element2 = in.nextInt();
 
         System.out.print("Въведете елемент 3: ");
-        double element3 = in.nextInt();
+        int element3 = in.nextInt();
 
-        double[] array = {element1, element2, element3};
+        int[] array = {element1, element2, element3};
         double sumAverage = array[0];
         double sum = 0;
         for (int i = array.length - 1; i >= 0; i--) {
---
+++ Lecture6/Homework3_middleChar.java
@@ -0,0 +1,35 @@
+package Lecture6;
+
+public class Homework3_middleChar {
+    //    Write a Java method to display the middle character of a string.
+    //    Note:
+    //    a) If the length of the string is even there will be two middle characters.
+    //    b) If the length of the string is odd there will be one middle character.
+    //    Write a Java method to count all words in a string.
+    public static String middleCharacter(String str) {
+
+        int length = str.length();
+        if (length % 2 == 0) {
+            int mid = length / 2;
+            return str.substring(mid - 1, mid + 1);
+        } else {
+            int mid = length / 2;
+            return String.valueOf(str.charAt(mid));
+        }
+    }
+    public static int countWords(String str) {
+        if (str == null || str.isEmpty()) {
+            return 0;
+        }
+
+        String[] words = str.split("\\s+");
+        return words.length;
+    }
+    public static void main(String[] args) {
+        String str = "Да живее България!";
+        System.out.println("Средният символ е/са: " + middleCharacter(str));
+
+        String sentence = "Да живее България! ";
+        System.out.println("Броят на думите е: " + countWords(sentence));
+    }
+}
