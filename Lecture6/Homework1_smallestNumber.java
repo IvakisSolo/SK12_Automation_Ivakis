8e529a53cf6fba32a41f2529209e59508a2a0abb
IvakisSolo <158038497+IvakisSolo@users.noreply.github.com>
2024-02-10 11:23:07 +0200

    Started Homework

---
+++ Lecture6/Homework1_smallestNumber.java
@@ -0,0 +1,5 @@
+package Lecture6;
+
+public class Homework1_smallestNumber {
+    // Write a Java method to find the smallest number among three numbers
+}
---
+++ Lecture6/Homework2_sumAverage.java
@@ -0,0 +1,5 @@
+package Lecture6;
+
+public class Homework2_sumAverage {
+    // Write a Java method to compute the average of three numbers.
+}
