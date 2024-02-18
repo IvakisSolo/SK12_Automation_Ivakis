83cfe0abe29d21c4abcc1a6d8b86123098cb3e27
IvakisSolo <158038497+IvakisSolo@users.noreply.github.com>
2024-02-18 14:14:15 +0200

    Homework1

---
+++ Lecture7/CarModel.java
@@ -0,0 +1,27 @@
+package Lecture7;
+
+public class CarModel {
+    public String carName;
+    public String carColor;
+    public int carReleaseYear;
+    public int carHorsePower;
+    public boolean boughtSecondHand;
+    public CarModel(String name, String color, int releaseYear, int horsePower, boolean secondHand) {
+        carName = name;
+        carColor = color;
+        carReleaseYear = releaseYear;
+        carHorsePower = horsePower;
+        boughtSecondHand = secondHand;
+    }
+    public CarModel(int releaseYear, int horsePower, boolean secondHand) {
+        this("N/A", "N/A", releaseYear, horsePower, secondHand);
+    }
+    public CarModel(String name, String color, boolean secondHand) {
+        this(name, color, -1, -1, secondHand);
+    }
+    public CarModel(String name, String color, int releaseYear, int horsePower) {
+        this(name, color, releaseYear, horsePower, false);
+    }
+    public CarModel() {
+    }
+}
